62ab18ca25f4a1c5e6a2c29f8f2ccf3bf8ac926b
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-01-10 10:05:15 -0800

    redshift stuff

--- macros/get_opportunity_tag_columns.sql
+++ macros/get_opportunity_tag_columns.sql
@@ -2,9 +2,14 @@
 
 {% set columns = [
     {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
-    {"name": "opportunity_id", "datatype": dbt_utils.type_string()},
-    {"name": "tag", "datatype": dbt_utils.type_string(),"quote": true}
+    {"name": "opportunity_id", "datatype": dbt_utils.type_string()}
 ] %}
+
+{% if target.type == 'redshift' %}
+ {{ columns.append( {"name": "tag", "datatype": dbt_utils.type_string(), "quote": True } ) }}
+{% else %}
+ {{ columns.append( {"name": "tag", "datatype": dbt_utils.type_string()} ) }}
+{% endif %}
 
 {{ return(columns) }}
 

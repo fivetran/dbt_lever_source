76e3cfebe7f43dd2918d40629a97e4dd35470825
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-01-10 09:43:44 -0800

    try quoting for redshift columns

--- macros/get_opportunity_tag_columns.sql
+++ macros/get_opportunity_tag_columns.sql
@@ -3,7 +3,7 @@
 {% set columns = [
     {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
     {"name": "opportunity_id", "datatype": dbt_utils.type_string()},
-    {"name": "tag", "datatype": dbt_utils.type_string()}
+    {"name": "tag", "datatype": dbt_utils.type_string(),"quote": true}
 ] %}
 
 {{ return(columns) }}

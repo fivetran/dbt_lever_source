2563a847ec25a5c36d6af863aad7ec112092fd4c
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-01-11 11:33:42 -0800

    just trying out quoting everything

--- macros/get_posting_tag_columns.sql
+++ macros/get_posting_tag_columns.sql
@@ -1,15 +1,15 @@
 {% macro get_posting_tag_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
-    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
-    {"name": "posting_id", "datatype": dbt_utils.type_string()}
+    {"name": "_fivetran_deleted", "datatype": "boolean", "quote": true},
+    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp(), "quote": true},
+    {"name": "posting_id", "datatype": dbt_utils.type_string(), "quote": true}
 ] %}
 
 {% if target.type == 'redshift' %}
  {{ columns.append( {"name": "tag", "datatype": dbt_utils.type_string(), "quote": True } ) }}
 {% else %}
- {{ columns.append( {"name": "tag", "datatype": dbt_utils.type_string()} ) }}
+ {{ columns.append( {"name": "tag", "datatype": dbt_utils.type_string(), "quote": true } ) }}
 {% endif %}
 
 {{ return(columns) }}

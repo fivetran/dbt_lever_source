77748a80f42d9f2178186c54a75c791d9596082c
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-10-17 17:33:54 -0500

    update changelog

--- models/tmp/stg_lever__interviewer_user_tmp.sql
+++ models/tmp/stg_lever__interviewer_user_tmp.sql
@@ -2,10 +2,10 @@
     fivetran_utils.union_data(
         table_identifier='interviewer_user', 
         database_variable='lever_database', 
-        schema_variable='lever_schema', 
+        schema_variable='interview_user' if var('lever__using_interview_user', lever_source.does_table_exist('interview_user')) else 'interviewer_user', 
         default_database=target.database,
         default_schema='lever',
-        default_variable='interviewer_user',
+        default_variable='interview_user' if var('lever__using_interview_user', lever_source.does_table_exist('interview_user')) else 'interviewer_user',
         union_schema_variable='lever_union_schemas',
         union_database_variable='lever_union_databases'
     )
--- models/tmp/stg_lever__user_tmp.sql
+++ models/tmp/stg_lever__user_tmp.sql
@@ -1,11 +1,11 @@
 {{
     fivetran_utils.union_data(
-        table_identifier='user', 
+        table_identifier='users' if var('lever__using_users', lever_source.does_table_exist('users')) else 'user', 
         database_variable='lever_database', 
         schema_variable='lever_schema', 
         default_database=target.database,
         default_schema='lever',
-        default_variable='user',
+        default_variable='users' if var('lever__using_users', lever_source.does_table_exist('users')) else 'user',
         union_schema_variable='lever_union_schemas',
         union_database_variable='lever_union_databases'
     )
